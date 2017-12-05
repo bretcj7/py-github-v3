a01b821661ebf38370e62175b9d7c5a5eb6256e1
Bret Askeland <baskeland@gmail.com>
2017-12-05 06:33:35 -0500

    Updates

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ import numpy as np
 import pandas as pd
 import requests
 
-API_KEY = 'a4d03a857807cb8fc26a3700e8f0a4daf393ba1d'
+# Get all org repos for lodash
+org_repos = requests.get('https://api.github.com/orgs/lodash/repos')
+df_repos = pd.DataFrame(org_repos.json(), columns=['name'])
+df_prs = pd.DataFrame()
 
-r = requests.get('https://api.github.com/orgs/lodash/repos')
-print (r.json()[0])
+def get_repo_prs(repo):
+    r = requests.get('https://api.github.com/repos/lodash/' + repo + '/pulls?state=all')
+    df_tmp = pd.DataFrame(r.json(), columns=['user'])
+    print (df_tmp)
+    df_prs.append(df_tmp)
+
+for r in range(0, df_repos.size):
+    get_repo_prs(df_repos.iloc[r]['name'])
+
+print (df_prs)
--- py_test.py
+++ py_test.py
@@ -5,11 +5,12 @@ import requests
 def test_http_get_repos():
     r = requests.get('https://api.github.com/orgs/lodash/repos')
     assert r.status_code == 200
-    assert len(r.json()) > 0
+    assert r.json()
 
 def test_http_get_pr():
-    r = requests.get('https://api.github.com/repos/lodash/lodash/pulls')
+    r = requests.get('https://api.github.com/repos/lodash/lodash/pulls?state=all')
     assert r.status_code == 200
+    assert r.json()
 
 def test_pandas_load():
     json_string = [{'id': 3955647, 'name': 'lodash', 'full_name': 'lodash/lodash'}]
