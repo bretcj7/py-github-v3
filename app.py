df827fce7c985a1b7d053ebb522412c0dbbee4df
Bret Askeland <baskeland@gmail.com>
2017-12-05 23:36:34 -0500

    Code refactor

--- app.py
+++ app.py
@@ -2,22 +2,32 @@
 import pandas as pd
 import requests
 
-# Using basic auth for ease of use right now
-USER_NAME = '<user' # CHANGE ME!
-USER_PWD = '<password>' # CHANGE ME!
+class OrgPullRequestInfo:
 
-# Get all org repos for lodash
-org_repos = requests.get('https://api.github.com/orgs/lodash/repos', auth=(USER_NAME, USER_PWD))
-# Store in memory data store of repo names
-df_repos = pd.DataFrame(org_repos.json(), columns=['name'])
+    # Using basic auth for ease of use right now
+    def __init__(self, org):
+        self.orgname = org
+        self.USER_NAME = '<user>' # CHANGE ME!
+        self.USER_PWD = '<password>' # CHANGE ME!
 
-# Get pull requests for a repository regarless of pull request state
-def get_repo_prs(repo):
-    response = requests.get('https://api.github.com/repos/lodash/' + repo + '/pulls?state=all', auth=(USER_NAME, USER_PWD))
-    return pd.DataFrame(response.json())
+    # Get all org repos for lodash
+    def __get_org_repos(self, org):
+        org_repos = requests.get(f'https://api.github.com/orgs/{org}/repos', auth=(self.USER_NAME, self.USER_PWD))
+        # Store in memory data store of repo names
+        return pd.DataFrame(org_repos.json(), columns=['name'])
 
-# Concat each repos list of pull requests into a single data store for further analysis
-all_prs = pd.concat([get_repo_prs(df_repos.iloc[i]['name']) for i in range(df_repos.size)], ignore_index=True)
+    # Get pull requests for a repository regarless of pull request state
+    def __get_repo_prs(self, repo):
+        response = requests.get(f'https://api.github.com/repos/lodash/{repo}/pulls?state=all', auth=(self.USER_NAME, self.USER_PWD))
+        return pd.DataFrame(response.json())
 
+    # Get pull request info for org
+    def get_pr_info(self):
+        org_repos = self.__get_org_repos(self.orgname)
+        # Concat each repos list of pull requests into a single data store for further analysis
+        return pd.concat([self.__get_repo_prs(org_repos.iloc[i]['name']) for i in range(org_repos.size)], ignore_index=True)
+
+pr = OrgPullRequestInfo('lodash')
+pr_output = pr.get_pr_info()
 # Print test output as json - records
-print (all_prs.to_json(orient='records'))
+print (pr_output.to_json(orient='records'))
