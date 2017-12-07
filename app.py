7fee83442dd39f06cbc73ac299d81c463f3a9804
Bret Askeland <baskeland@gmail.com>
2017-12-06 22:18:26 -0500

    add more unit tests

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import requests
 
 class OrgPullRequestInfo:
-
     # Using basic auth for ease of use right now
     def __init__(self, org):
         self.orgname = org
@@ -11,23 +10,24 @@ class OrgPullRequestInfo:
         self.USER_PWD = '<password>' # CHANGE ME!
 
     # Get all org repos for lodash
-    def __get_org_repos(self, org):
+    def get_org_repos(self, org):
         org_repos = requests.get(f'https://api.github.com/orgs/{org}/repos', auth=(self.USER_NAME, self.USER_PWD))
         # Store in memory data store of repo names
         return pd.DataFrame(org_repos.json(), columns=['name'])
 
     # Get pull requests for a repository regarless of pull request state
-    def __get_repo_prs(self, repo):
+    def get_repo_prs(self, repo):
         response = requests.get(f'https://api.github.com/repos/lodash/{repo}/pulls?state=all', auth=(self.USER_NAME, self.USER_PWD))
         return pd.DataFrame(response.json())
 
     # Get pull request info for org
     def get_pr_info(self):
-        org_repos = self.__get_org_repos(self.orgname)
+        org_repos = self.get_org_repos(self.orgname)
         # Concat each repos list of pull requests into a single data store for further analysis
-        return pd.concat([self.__get_repo_prs(org_repos.iloc[i]['name']) for i in range(org_repos.size)], ignore_index=True)
+        return pd.concat([self.get_repo_prs(org_repos.iloc[i]['name']) for i in range(org_repos.size)], ignore_index=True)
 
-pr = OrgPullRequestInfo('lodash')
-pr_output = pr.get_pr_info()
-# Print test output as json - records
-print (pr_output.to_json(orient='records'))
+if __name__ == '__main__':
+    pr = OrgPullRequestInfo('lodash')
+    pr_output = pr.get_pr_info()
+    # Print test output as json - records
+    print (pr_output.to_json(orient='records'))
--- py_test.py
+++ py_test.py
@@ -1,21 +1,30 @@
+import app as tc
 import pytest
 import pandas as pd
 import requests
 
+# Define test fixture so class will be instantiated once per module
+@pytest.fixture(scope="module")
+def test_class():
+    return tc.OrgPullRequestInfo('lodash')
+
+# Test new class is of instance
+def test_new_pr_class_init(test_class):
+    assert isinstance(test_class, tc.OrgPullRequestInfo)
+
 # Test basic get request of org's repos
-def test_http_get_repos():
-    r = requests.get('https://api.github.com/orgs/lodash/repos')
-    assert r.status_code == 200
-    assert r.json()
+def test_get_org_repos(test_class):
+    assert test_class.get_org_repos('lodash').to_json()
 
 # Test basic get request of pull requests for an org/repo
-def test_http_get_pr():
-    r = requests.get('https://api.github.com/repos/lodash/lodash/pulls?state=all')
-    assert r.status_code == 200
-    assert r.json()
+def test_get_repo_prs(test_class):
+    assert test_class.get_repo_prs('lodash').to_json()
 
 # Test basic data store of json to dataframe
 def test_pandas_load():
     json_string = [{'id': 3955647, 'name': 'lodash', 'full_name': 'lodash/lodash'}]
+    json_string2 = [{'id': 908, 'name': 'test', 'full_name': 'lodash/test', 'other': 'other'}]
     df = pd.DataFrame(json_string,  columns=['name','full_name'])
+    df2 = pd.DataFrame(json_string2, columns=['name','full_name'])
     assert df.size == 2
+    assert pd.concat([df, df2], ignore_index=True).empty == False
