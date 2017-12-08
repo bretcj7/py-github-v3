5091a1c0e375a22046feeaa6362a293211de5fd5
Bret Askeland <baskeland@gmail.com>
2017-12-07 22:25:46 -0500

    update based on pagination in api

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 """ github REST API v3 - https://developer.github.com/v3/ """
 import pandas as pd
+import re
 import requests
 
 class OrgPullRequestInfo:
+    page_pattern = re.compile('(?<=&page=)(.*?)(?=\>)')
+
     # Using basic auth for ease of use right now
     def __init__(self, org):
         self.orgname = org
-        self.USER_NAME = '<user>' # CHANGE ME!
-        self.USER_PWD = '<password>' # CHANGE ME!
+        self.USER_NAME = 'bretcj7' # CHANGE ME!
+        self.USER_PWD = 'Marley04' # CHANGE ME!
 
     # Get all org repos for lodash
     def get_org_repos(self, org):
@@ -17,8 +20,17 @@ class OrgPullRequestInfo:
 
     # Get pull requests for a repository regarless of pull request state
     def get_repo_prs(self, repo):
-        response = requests.get(f'https://api.github.com/repos/lodash/{repo}/pulls?state=all', auth=(self.USER_NAME, self.USER_PWD))
-        return pd.DataFrame(response.json())
+        response = requests.get(f'https://api.github.com/repos/lodash/{repo}/pulls?state=all&per_page=100', auth=(self.USER_NAME, self.USER_PWD))
+        page_max = self.page_pattern.findall(str(response.headers))
+        df = pd.DataFrame()
+        # Check if there is pagination to this response
+        if page_max:
+            for i in range(1, int(page_max[-1])+1):
+                response = requests.get(f'https://api.github.com/repos/lodash/{repo}/pulls?state=all&page={i}&per_page=100', auth=(self.USER_NAME, self.USER_PWD))
+                df = pd.concat([df, pd.DataFrame(response.json())], ignore_index=True)
+        else:
+            df = pd.DataFrame(response.json())
+        return df
 
     # Get pull request info for org
     def get_pr_info(self):
@@ -29,5 +41,5 @@ class OrgPullRequestInfo:
 if __name__ == '__main__':
     pr = OrgPullRequestInfo('lodash')
     pr_output = pr.get_pr_info()
-    # Print test output as json - records
-    print (pr_output.to_json(orient='records'))
+    # Print test output as number of pull requests
+    print (pr_output.count()['id'])
